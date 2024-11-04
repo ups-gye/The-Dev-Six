476822d4ec55272c95eae6ff598a87a6c3bf76d1
AdrianC47 <luiscabreraucme@hotmail.com>
2024-11-04 01:06:04 -0500

    crud ok

--- proyecto_01/frontend/coreui-free-angular-admin-template-main/src/app/services/product.service.ts
+++ proyecto_01/frontend/coreui-free-angular-admin-template-main/src/app/services/product.service.ts
@@ -32,12 +32,51 @@ export class ProductService {
         codigoProducto,
         nombreProducto,
         stock,
-        precioUnitario
+        precioUnitario,
+        categoria,
+        subcategoria,
+        proveedor
       }
     }`
     };
     console.log(body);
     return this.http.post(this.url, body);
   }
+
+  updateProduct(product: any) {
+    const body = {
+      query: `mutation {
+      updateProduct(productId: "${product.codigoProducto}", product: {
+        codigoProducto: "${product.codigoProducto}",
+        nombreProducto: "${product.nombreProducto}",
+        stock: ${product.stock},
+        precioUnitario: ${product.precioUnitario},
+        categoria: "${product.categoria}",
+        subcategoria: "${product.subcategoria}",
+        proveedor: "${product.proveedor}"
+      }) {
+        codigoProducto,
+        nombreProducto,
+        stock,
+        precioUnitario,
+        categoria,
+        subcategoria,
+        proveedor
+      }
+    }`
+    };
+    console.log(body);
+    return this.http.post(this.url, body);
+  }
+
+  deleteProduct(productId: string) {
+    const body = {
+      query: `mutation {
+      deleteProduct(productId: "${productId}")
+    }`
+    };
+    return this.http.post(this.url, body);
+  }
+
 
 }
--- proyecto_01/frontend/coreui-free-angular-admin-template-main/src/app/views/listar-productos/listar-productos.component.ts
+++ proyecto_01/frontend/coreui-free-angular-admin-template-main/src/app/views/listar-productos/listar-productos.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {
+  ButtonCloseDirective,
   ButtonDirective,
   CardBodyComponent,
   CardComponent,
   CardHeaderComponent,
-  ColComponent,
+  ColComponent, FormControlDirective, FormLabelDirective,
+  ModalBodyComponent,
+  ModalComponent,
+  ModalFooterComponent,
+  ModalHeaderComponent,
+  ModalTitleDirective,
+  ModalToggleDirective, RowComponent,
   TableColorDirective,
   TableDirective
 } from "@coreui/angular";
 import {Product} from "../../models/Product";
 import {ProductService} from "../../services/product.service";
-import { CommonModule } from '@angular/common';
-import { freeSet } from '@coreui/icons'; // Importar solo el conjunto de iconos gratuitos
-import {IconComponent, IconDirective, IconSetService,IconModule} from '@coreui/icons-angular';
+import {CommonModule} from '@angular/common';
+import {freeSet} from '@coreui/icons'; // Importar solo el conjunto de iconos gratuitos
+import {IconComponent, IconDirective, IconSetService, IconModule} from '@coreui/icons-angular';
+import {FormsModule, NgForm} from "@angular/forms";
+
 @Component({
   selector: 'app-listar-productos',
   standalone: true,
@@ -27,19 +36,44 @@ import {IconComponent, IconDirective, IconSetService,IconModule} from '@coreui/i
     IconDirective,
     ButtonDirective,
     IconComponent,
-    IconModule
+    IconModule,
+    ModalHeaderComponent,
+    ModalComponent,
+    ModalBodyComponent,
+    ModalFooterComponent,
+    ModalToggleDirective,
+    ModalTitleDirective,
+    ButtonCloseDirective,
+    FormsModule,
+    FormControlDirective,
+    FormLabelDirective,
+    RowComponent
   ],
   templateUrl: './listar-productos.component.html',
   styleUrl: './listar-productos.component.scss',
   providers: [IconSetService]
 })
 export class ListarProductosComponent {
-  productos: Product[]=[];
-  icons = { ...freeSet };
-  constructor(private productService:ProductService, ) {
-  }
-ngOnInit(): void {
+  productos: Product[] = [];
+  icons = {...freeSet};
+  selectedProduct: any = {};
+  selectedProductForDeletion: any = {};
+  modalMessage = '';
+  modalVisible = false;
+  confirmDeleteModalVisible = false; // Controla la visibilidad del modal de confirmación de eliminación
 
+
+  constructor(private productService: ProductService,) {
+    this.getProducts();
+
+  }
+
+  ngOnInit(): void {
+  this.getProducts();
+    this.modalVisible = false;
+  }
+
+  getProducts() {
     const query = {
       query: `
         {
@@ -56,10 +90,11 @@ ngOnInit(): void {
       `
     };
 
-   this.productService.getProducts(query).subscribe(
+
+    this.productService.getProducts(query).subscribe(
       (response: any) => {
         if (response && response.data && response.data.allProducts) {
-          this.productos = response.data.allProducts; // Assign the products to the array
+          this.productos = response.data.allProducts.sort((a: any, b: any) => a.codigoProducto.localeCompare(b.codigoProducto));
         } else {
           console.error("The response does not contain products:", response);
         }
@@ -69,4 +104,65 @@ ngOnInit(): void {
       }
     );
   }
+
+  openDeleteModal(product: any) {
+    this.selectedProductForDeletion = product;
+    this.confirmDeleteModalVisible = true;
+  }
+
+  confirmDeleteProduct() {
+    const productId = this.selectedProductForDeletion.codigoProducto;
+    this.productService.deleteProduct(productId).subscribe(response => {
+      console.log('Producto eliminado:', response);
+
+      // Actualizar la lista de productos después de la eliminación
+      this.getProducts();
+
+      // Cerrar el modal de confirmación y mostrar un mensaje de éxito
+      this.confirmDeleteModalVisible = false;
+      this.modalMessage = 'Producto eliminado exitosamente';
+      this.modalVisible = true;
+
+    }, error => {
+      console.error('Error al eliminar el producto:', error);
+      this.modalMessage = 'Error al eliminar el producto';
+      this.modalVisible = true;
+    });
+  }
+  updateProduct(form: any) {
+    if (form.valid) {
+      this.productService.updateProduct(this.selectedProduct).subscribe(response => {
+        console.log('Producto actualizado:', response);
+        this.getProducts();
+
+        document.getElementById('closeEditModalButton')?.click();
+
+        this.modalMessage = 'Producto actualizado exitosamente';
+        this.modalVisible = true;
+
+        form.resetForm();
+        this.selectedProduct = {}; // Limpia el producto seleccionado
+
+
+
+      }, error => {
+        console.error('Error al actualizar el producto:', error);
+        this.modalMessage = 'Error al actualizar el producto';
+        this.modalVisible = true;
+      });
+    }
+    else {
+      this.modalMessage = "Ocurrió un error al actualizar el producto. Por favor, intenta nuevamente.";
+      this.modalVisible = true;
+    }
+  }
+
+  setSelectedProduct(product: any) {
+    this.selectedProduct = { ...product };
+
+  }
+  closeModal() {
+    this.modalVisible = false;
+  }
+
 }
